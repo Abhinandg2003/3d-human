38bb8e2857f2e96b19eb82e537373a33a0a7e4be
Abhinandg2003 <abhinandg2003@gmail.com>
2026-09-16 15:30:20 +0530

    dynamic coloring and dya data remove src

--- src - Copy/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ModelScene from "@/components/ModelScene";
-
-export default function Home() {
-  return (
-    <main className="relative flex-1 bg-white">
-      <ModelScene />
-    </main>
-  );
-}
